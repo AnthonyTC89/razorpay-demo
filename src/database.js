b3983e069da3d0cb91688c8c8a3a369ad15affd4
AnthonyTC89 <ptonyptc@gmail.com>
2020-08-13 17:54:43 -0500

    database setup done; express initialized

---
+++ src/database.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const USERNAME = process.env.USERNAME_MONGODB;
+const PASSWORD = process.env.PASSWORD_MONGODB;
+const CLUSTER = process.env.CLUSTER_MONGODB;
+const DATABASE = process.env.DATABASE_MONGODB;
+
+const uri = `mongodb+srv://${USERNAME}:${PASSWORD}@${CLUSTER}/${DATABASE}?retryWrites=true&w=majority`;
+
+const options = {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(uri, options)
+  .then(() => {
+    console.log('Successful connection MongoDB Atlas');
+  })
+  .catch((err) => {
+    console.log('Unsuccessful connection MongoDB Atlas', err);
+  });
+
+module.exports = mongoose;
